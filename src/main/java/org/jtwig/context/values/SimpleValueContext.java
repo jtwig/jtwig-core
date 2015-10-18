aa06927bab6cc7781863a4fbd504e360afd48e4d
Joao De Melo <jmelo@lyncode.com>
2015-10-19 00:18:11 +0100

    Validating keys

--- src/main/java/org/jtwig/context/values/SimpleValueContext.java
+++ src/main/java/org/jtwig/context/values/SimpleValueContext.java
@@ -1,11 +1,13 @@
 package org.jtwig.context.values;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import org.jtwig.reflection.model.Value;
 
 import java.util.Map;
 
 public class SimpleValueContext implements ValueContext {
+    public static final String IDENTIFIER_REGEX = "[a-zA-Z_$][a-zA-Z0-9_$]*";
     private final Map<String, Value> source;
 
     public SimpleValueContext(Map<String, Value> source) {
@@ -19,6 +21,7 @@ public class SimpleValueContext implements ValueContext {
 
     @Override
     public SimpleValueContext add(String key, Object value) {
+        Preconditions.checkArgument(key.matches(IDENTIFIER_REGEX), String.format("Argument %s isn't a valid identifier. It must conform to the following regex: %s", key, IDENTIFIER_REGEX));
         source.put(key, new Value(value));
         return this;
     }
