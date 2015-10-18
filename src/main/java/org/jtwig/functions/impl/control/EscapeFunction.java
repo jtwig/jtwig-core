95077673f914f7c5b9204ca31542a8d470779aa0
Joao De Melo <jmelo@lyncode.com>
2015-10-19 00:44:18 +0100

    Improved error information in escape function

--- src/main/java/org/jtwig/functions/impl/control/EscapeFunction.java
+++ src/main/java/org/jtwig/functions/impl/control/EscapeFunction.java
@@ -1,10 +1,16 @@
 package org.jtwig.functions.impl.control;
 
+import com.google.common.base.Function;
+import com.google.common.base.Supplier;
+import com.google.common.collect.Collections2;
+import org.apache.commons.lang3.StringUtils;
 import org.jtwig.context.RenderContext;
 import org.jtwig.context.RenderContextHolder;
 import org.jtwig.context.model.EscapeMode;
 import org.jtwig.functions.JtwigFunctionRequest;
 import org.jtwig.functions.SimpleJtwigFunction;
+
+import static java.util.Arrays.asList;
 
 public class EscapeFunction extends SimpleJtwigFunction {
     @Override
@@ -13,17 +19,34 @@ public class EscapeFunction extends SimpleJtwigFunction {
     }
 
     @Override
-    public Object execute(JtwigFunctionRequest request) {
+    public Object execute(final JtwigFunctionRequest request) {
         request.minimumNumberOfArguments(1);
         request.maximumNumberOfArguments(2);
 
         EscapeMode escapeMode = EscapeMode.HTML;
         if (request.getNumberOfArguments() == 2) {
-            escapeMode = EscapeMode.valueOf(request.getArgument(1, String.class).toUpperCase());
+            final String requestedEscapeMode = request.getArgument(1, String.class);
+            escapeMode = EscapeMode.fromString(requestedEscapeMode.toUpperCase())
+                .or(throwError(request, requestedEscapeMode));
         }
 
         getRenderContext().currentNode().mode(escapeMode);
         return request.getArgument(0, Object.class);
+    }
+
+    private Supplier<EscapeMode> throwError(final JtwigFunctionRequest request, final String requestedEscapeMode) {
+        return new Supplier<EscapeMode>() {
+            @Override
+            public EscapeMode get() {
+                String possibleEscapeModes = StringUtils.join(Collections2.transform(asList(EscapeMode.values()), new Function<EscapeMode, String>() {
+                    @Override
+                    public String apply(EscapeMode input) {
+                        return input.toString();
+                    }
+                }), ", ");
+                throw request.exception(String.format("Invalid escape mode requested '%s'. Only supporting [%s]", requestedEscapeMode, possibleEscapeModes));
+            }
+        };
     }
 
     protected RenderContext getRenderContext() {
