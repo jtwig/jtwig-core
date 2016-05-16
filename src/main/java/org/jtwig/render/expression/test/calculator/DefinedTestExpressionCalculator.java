2deee3a582159b617a6c62b65da0a15f3076cf82
Joao De Melo <jmelo@lyncode.com>
2016-05-16 21:44:56 +0100

    Fixing defined test expression calculator

--- src/main/java/org/jtwig/render/expression/test/calculator/DefinedTestExpressionCalculator.java
+++ src/main/java/org/jtwig/render/expression/test/calculator/DefinedTestExpressionCalculator.java
@@ -1,26 +1,22 @@
 package org.jtwig.render.expression.test.calculator;
 
+import org.jtwig.exceptions.ResolveValueException;
 import org.jtwig.model.expression.Expression;
-import org.jtwig.model.expression.VariableExpression;
 import org.jtwig.model.expression.test.DefinedTestExpression;
 import org.jtwig.model.position.Position;
 import org.jtwig.render.RenderRequest;
+import org.jtwig.render.expression.CalculateExpressionService;
 import org.jtwig.value.Undefined;
-import org.jtwig.value.context.ValueContext;
 
 public class DefinedTestExpressionCalculator implements TestExpressionCalculator<DefinedTestExpression> {
     @Override
     public Object calculate(RenderRequest request, Position position, DefinedTestExpression test, Expression argument) {
-        if (argument instanceof VariableExpression) {
-            ValueContext current = request.getRenderContext().getValueContext().getCurrent();
-            Object result = current.resolve(((VariableExpression) argument).getIdentifier());
-            if (result == null || result == Undefined.UNDEFINED) {
-                return false;
-            } {
-                return true;
-            }
-        } else {
-            return true;
+        CalculateExpressionService calculateExpressionService = request.getEnvironment().getRenderEnvironment().getCalculateExpressionService();
+        try {
+            Object value = calculateExpressionService.calculate(request, argument);
+            return value != null && value != Undefined.UNDEFINED;
+        } catch (ResolveValueException e) {
+            return false;
         }
     }
 }
