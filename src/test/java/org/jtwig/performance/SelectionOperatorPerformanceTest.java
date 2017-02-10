7be14daa3b4f464d244b44f46b3b1e1265a0a39e
Joao De Melo <jmelo@lyncode.com>
2017-02-10 19:26:57 +0000

    Selection operation

--- src/test/java/org/jtwig/performance/SelectionOperatorPerformanceTest.java
+++ src/test/java/org/jtwig/performance/SelectionOperatorPerformanceTest.java
@@ -6,12 +6,15 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 import org.jtwig.integration.expression.SelectionTest;
+import org.jtwig.resource.loader.InMemoryResourceLoader;
+import org.jtwig.resource.loader.TypedResourceLoader;
 import org.junit.Test;
 
 import static org.jtwig.JtwigModel.newModel;
+import static org.jtwig.environment.EnvironmentConfigurationBuilder.configuration;
 
 public class SelectionOperatorPerformanceTest {
-    public static final int SIZE = 100000;
+    public static final int SIZE = 10000;
 
     @Test
     public void select() throws Exception {
@@ -31,5 +34,33 @@ public class SelectionOperatorPerformanceTest {
 
         groupedTimingStatistics.setStopTime(System.currentTimeMillis());
         System.out.println(groupedTimingStatistics.toString());
+    }
+
+    public static void main(String[] args) {
+        JtwigTemplate jtwigTemplate = JtwigTemplate.inlineTemplate("{% include 'm:a' %}" +
+                "\n" +
+                "{% if (var is defined) %}" +
+                "{{ var.nested.field[0] }}" +
+                "{% endif %}" +
+                "\n" +
+                "{% for k, v in { test: 'one', test1: 'false' } %}" +
+                "{{ k }} => {{ v }}\n" +
+                "{% endfor %}" +
+                "\n" +
+                "{% include 'm:b' %}", configuration()
+                .resources().resourceLoaders().add(new TypedResourceLoader("m", InMemoryResourceLoader.builder()
+                        .withResource("a", "This is something cool")
+                        .withResource("b", "This is something boring")
+                        .build()))
+                .and().and()
+                .build());
+
+        System.out.println(jtwigTemplate.render(newModel().with("var", new SelectionTest.TestClass(RandomStringUtils.randomAlphabetic(6)))));
+
+        while (true) {
+            String random = RandomStringUtils.random(6);
+            JtwigModel jtwigModel = newModel().with("var", new SelectionTest.TestClass(random));
+            jtwigTemplate.render(jtwigModel);
+        }
     }
 }
